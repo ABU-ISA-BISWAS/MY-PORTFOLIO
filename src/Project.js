ce024d1c6ec133eb21dd55b9bd12822bf912c8b4
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-06-09 23:36:03 +0600

    home page

---
+++ src/Project.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Project.css";
+import Project1 from './img/aicomputer.png';
+import Project2 from './img/fruit.png';
+import Project3 from './img/dental.png';
+
+
+function Project() {
+  return (
+    <div className="project component__space" id="Portfolio">
+      <div className="heading">
+        <h1 className="heading">My Latest Project</h1>
+        
+      </div>
+       <div className="container">
+           <div className="row">
+             
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project1} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">AI-Computer</h5>
+                     
+                     <a href="https://computer-point-7ec78.web.app/" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project2} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Fruit-warehouse</h5>
+                     
+                     <a href="https://fruits-warehouse-fffdc.web.app/" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project3} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Dental-Doctor</h5>
+                    
+                     <a href="https://dental-services-a744c.web.app/" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+
+
+             
+             <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
+             <button className="view__more pointer btn">View more</button>
+             </div>
+
+           </div>
+       </div>
+    </div>
+  );
+}
+
+export default Project;
