1788ae89f175606b35690d460fb7495f68c51b74
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-06-11 18:55:45 +0600

    blogs

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div className='bg-white text-center  mt-10'>
-            <h1 className='text-blue-600 font-bold text-5xl mb-10'>Comming Soon...</h1>
-            <button  className=" button pointer mt-10"><Link to="/">Go Back To Home</Link> </button>
+        <div className='bg text-center  '>
+            <h2 className='text-blue-600 font-bold text-5xl mb-10'>Comming Soon...</h2>
+            <button  className=" buttn pointer mt-10 mb-10"><Link to="/">Go Back To Home</Link> </button>
         </div>
     );
 };
